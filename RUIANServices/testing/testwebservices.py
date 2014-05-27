97f11263b87352f4836b781dfcea182198962107
Radek Augustýn <raugustyn@post.cz>
2014-05-27 05:33:13 +0200

    Closes #45

---
+++ RUIANServices/testing/testwebservices.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'raugustyn'
+
+import unittest
+import RUIANServices.services.compileaddress_UnitTests
+
+class TestGlobalFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testusage(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
