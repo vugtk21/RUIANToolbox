c21198707f61ccfb33f21b489ca706f5a58b7196
Radek Augustýn <raugustyn@post.cz>
2014-06-01 13:54:06 +0200

    Parse address update

--- RUIANServices/services/parseaddress.py
+++ RUIANServices/services/parseaddress.py
@@ -27,7 +27,7 @@ class RUIANDatabase():
 
     def getObecByName(self, name):
         cursor = self.conection.cursor()
-        cursor.execute("SELECT obce FROM obce WHERE nazev_obce ilike '%" + name + "%'")
+        cursor.execute("SELECT obce FROM obce WHERE nazev_obce ilike '%" + name + "%' limit 25")
 
         rows = []
         row_count = 0
@@ -40,7 +40,7 @@ class RUIANDatabase():
 
     def getUliceByName(self, name):
         cursor = self.conection.cursor()
-        cursor.execute("SELECT nazev_ulice FROM ulice WHERE nazev_ulice ilike '%" + name + "%'")
+        cursor.execute("SELECT nazev_ulice FROM ulice WHERE nazev_ulice ilike '%" + name + "%' limit 25")
 
         rows = []
         row_count = 0
@@ -51,18 +51,91 @@ class RUIANDatabase():
               rows.append(data)
         return rows
 
+    def getCastObceByName(self, name):
+        cursor = self.conection.cursor()
+        cursor.execute("SELECT nazev_casti_obce FROM casti_obce WHERE nazev_casti_obce ilike '%" + name + "%' limit 25")
+
+        rows = []
+        row_count = 0
+        for row in cursor:
+              row_count += 1
+              data = row[0]
+              data = data[1:len(data) - 1]
+              rows.append(data)
+        return rows
+
+ruianDatabase = None
+
 def isInt(value):
-    try:
-        v = int(value)
-        return True
-    except ValueError:
+    if value == "" or value == None:
         return False
+    else:
+        for i in range(len(value)):
+            if "0123456789".find(value[i:i + 1]) < 0:
+                return False
+        return True
+    #try:
+    #    v = int(value)
+    #    return True
+    #except ValueError:
+    #    return False
+
+
+ORIENTATION_NUMBER_SEPARATOR = "/"
+RECORD_NUMBER_ID = "č.ev."
+DESCRIPTION_NUMBER_ID = "č.p."
+
+class AddressItem:
+    def __init__(self, value):
+        self.value = value
+        self.town = None
+        self.townPart = None
+        self.street = None
+        self.isRecordNumber = False
+        self.isOrientationNumber = False
+        self.isZIP = False
+        self.isHouseNumber = False
+        self.number = None
+        self.orientationNumberID = False
+        self.recordNumberID = False
+        self.analyseValue()
+
+    def __repr__(self):
+        result = ""
+        if self.isZIP: result += "PSČ "
+
+        if self.isOrientationNumber: result += ORIENTATION_NUMBER_SEPARATOR
+
+        if self.isRecordNumber: result += RECORD_NUMBER_ID + " "
+
+        #if self.isHouseNumber: result += DESCRIPTION_NUMBER_ID + " "
+
+        if self.number == None:
+            result += '"' + self.value + '"'
+        else:
+            result += self.number
+
+        return result
+
+    def analyseValue(self):
+        if isInt(self.value):
+            self.number = self.value
+            pass
+        elif self.value == ORIENTATION_NUMBER_SEPARATOR:
+            self.orientationNumberID = True
+        elif self.value == RECORD_NUMBER_ID:
+            self.recordNumberID = True
+        else:
+            self.town = ruianDatabase.getObecByName(self.value)
+            self.street = ruianDatabase.getUliceByName(self.value)
+            self.townPart = ruianDatabase.getCastObceByName(self.value)
 
 class AddressParser:
-    NUMBER_CHARS = "0123456789"
-    ORIENTATION_NUMBER_SEPARATOR = "/"
-    DESCRIPTION_NUMBER_ID = "č.p."
-    RECORD_NUMBER_ID = "č.ev."
+    RECORD_NUMBER_MAX_LEN = 4
+    ORIENTATION_NUMBER_MAX_LEN = 3
+    HOUSE_NUMBER_MAX_LEN = 4
+    ZIPCODE_LEN = 5
+
 
     def normaliseSeparators(self, address):
         address = address.replace("  ", " ")
@@ -80,31 +153,29 @@ class AddressParser:
         return address
 
     def normaliseDescriptionNumberID(self, address):
-        address = address.replace("čp ", self.DESCRIPTION_NUMBER_ID + " ")
-        address = address.replace("čp.", self.DESCRIPTION_NUMBER_ID)
+        address = address.replace("čp ", DESCRIPTION_NUMBER_ID + " ")
+        address = address.replace("čp.", DESCRIPTION_NUMBER_ID)
         return address
 
-    def expandNadPod(self, address):
+    def expandNad(self, address):
         address = address.replace(" n ", " nad ")
         address = address.replace(" n.", " nad ")
-        address = address.replace(" p ", " pod ")
-        address = address.replace(" p.", " pod ")
         return address
 
     def normaliseRecordNumberID(self, address):
-        address = address.replace("ev.č.",  self.RECORD_NUMBER_ID)
-        address = address.replace("ev č.",  self.RECORD_NUMBER_ID)
-        address = address.replace("evč.",   self.RECORD_NUMBER_ID)
-        address = address.replace("eč.",    self.RECORD_NUMBER_ID)
-        address = address.replace("ev. č.", self.RECORD_NUMBER_ID)
-        address = address.replace("č. ev.", self.RECORD_NUMBER_ID)
-        address = address.replace("čev.",   self.RECORD_NUMBER_ID)
+        address = address.replace("ev.č.",  RECORD_NUMBER_ID)
+        address = address.replace("ev č.",  RECORD_NUMBER_ID)
+        address = address.replace("evč.",   RECORD_NUMBER_ID)
+        address = address.replace("eč.",    RECORD_NUMBER_ID)
+        address = address.replace("ev. č.", RECORD_NUMBER_ID)
+        address = address.replace("č. ev.", RECORD_NUMBER_ID)
+        address = address.replace("čev.",   RECORD_NUMBER_ID)
         return address
 
     def separateNumbers(self, address):
         newAddress = ""
         wasNumber = False
-        for i in range(0, len(address)):
+        for i in range(len(address)):
             actChar = address[i:i+1]
             if "0123456789".find(actChar) >= 0:
                 if i > 0 and not wasNumber:
@@ -120,43 +191,104 @@ class AddressParser:
         return self.normaliseSeparators(newAddress)
 
     def normalize(self, address):
-        parsed = []
-        self.number = ""
-
         address = self.normaliseSeparators(address)
         address = self.normaliseDescriptionNumberID(address)
         address = self.normaliseRecordNumberID(address)
-        address = self.expandNadPod(address)
+        address = self.expandNad(address)
         address = self.separateNumbers(address)
 
         return address
 
-    def search(self, address):
+    def parse(self, address):
         address = self.normalize(address)
-        items = address.split(",")
+        stringItems = address.split(",")
+        items = []
+        for value in stringItems:
+            item = AddressItem(value)
+            items.append(item)
+        return items
 
-        db = RUIANDatabase()
+    def analyseItems(self, items):
+        newItems = []
+        index = 0
+        nextItemToBeSkipped = False
         for item in items:
-            if isInt(item):
-                pass
-            elif item == self.ORIENTATION_NUMBER_SEPARATOR:
-                pass
-            elif item == self.RECORD_NUMBER_ID:
-                pass
+            if nextItemToBeSkipped:
+                nextItemToBeSkipped = False
+                continue
+
+            if index == len(items) - 1:
+                nextItem = None
             else:
-                obce = db.getObecByName(item)
-                if obce <> []:
-                    print "Obec:", obce
-                ulice = db.getUliceByName(item)
-                if ulice <> []:
-                    print "ulice:", ulice
-        db = None
-        print items
+                nextItem = items[index + 1]
+
+            toBeSkipped = False
+            if item.orientationNumberID or item.recordNumberID:
+                if item.orientationNumberID:
+                    maxLen = self.ORIENTATION_NUMBER_MAX_LEN
+                else:
+                    maxLen = self.RECORD_NUMBER_MAX_LEN
+
+                if nextItem == None or nextItem.number == None or len(nextItem.number) > maxLen:
+                    toBeSkipped = True
+                    # Error, za indikátorem č.ev. nebo č.or. nenásleduje číslice
+                else:
+                    item.number = nextItem.number
+                    nextItemToBeSkipped = True
+                    if item.orientationNumberID:
+                        item.isOrientationNumber = True
+                    else:
+                        item.isRecordNumber = True
+
+            elif item.number != None:
+                if len(item.number) == 3 and nextItem.number != None and len(nextItem.number) == 2:
+                    item.number += nextItem.number
+                    nextItemToBeSkipped = True
+
+                if len(item.number) == self.ZIPCODE_LEN:
+                    item.isZIP = True
+                elif len(item.number) <= self.HOUSE_NUMBER_MAX_LEN:
+                    item.isHouseNumber = True
+                else:
+                    # Error, příliš dlouhé číslo domovní nebo evidenční
+                    pass
+            else:
+                #else textový řetezec
+                pass
+
+            if not toBeSkipped:
+                newItems.append(item)
+
+            index = index + 1
+
+        return newItems
 
 
-parser = AddressParser()
-parser.search("Pod lesem 1370 č. ev. 1530, Březová")
-parser.search("Brněnská 1370 č. p. 113, Březová")
-parser.search("Pod lesem 1370 č. ev. 1530 Roudnice nad Labem")
-parser.search("Pod lesem 1370 č. ev. 1530 Svatý Jan p skalou")
-parser.search("Pod lesem 1370 č. ev. 1530 Lipník n ")
+    def analyse(self, address):
+        items = self.parse(address)
+        return self.analyseItems(items)
+
+    def testAnalyse(self, address, expectedResult):
+        result = self.analyse(address)
+        if str(result) == expectedResult:
+            print "   ok :", address, "-->", result
+        else:
+            print "chyba :", address, "-->", result, "<>", expectedResult
+
+def initModule():
+    global ruianDatabase
+    ruianDatabase = RUIANDatabase()
+
+def main():
+    initModule()
+    parser = AddressParser()
+    #parser.testAnalyse("16300", "[PSČ 16300]")
+    #parser.testAnalyse("163 00", "[PSČ 16300]")
+    #parser.testAnalyse("Pod lesem 1370 č. ev. 1530, Březová", '["Pod lesem", 1370, č.ev. 1530, "Březová"]')
+    parser.testAnalyse("Brněnská 1370 č. p. 113, Březová", '["Brněnská", 1370, "č.p.", 113, "Březová"]')
+    #parser.testAnalyse("Pod lesem 1370 č. ev. 1530 Roudnice nad Labem", '["Pod lesem", 1370, č.ev. 1530, "Roudnice nad Labem"]')
+    #parser.testAnalyse("Pod lesem 1370 č. ev. 1530 Svatý Jan p skalou", '["Pod lesem", 1370, č.ev. 1530, "Svatý Jan pod skalou"]')
+    #parser.testAnalyse("Pod lesem 1370 č. ev. 1530 Lipník n ", '["Pod lesem", 1370, č.ev. 1530, "Lipník nad "]')
+
+if __name__ == '__main__':
+    main()
