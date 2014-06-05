c33ad35f717776223cd617f47d3ff8471f887a02
vacektom <mr.vaca@seznam.cz>
2014-06-05 09:14:33 +0200

    doplněny příklady zadávání adres k otestování

--- RUIANServices/testing/geocode.py
+++ RUIANServices/testing/geocode.py
@@ -18,8 +18,19 @@ def test(testerParam = None):
                 "Compiling person", "Tester")
 
     tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=1408739", "1033052.61, 766195.05")
+    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=20388802", "1056492.96, 529426.07")
+    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=8123934", "1098618.98, 568885.13")
+    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=8150656", "1086263.12, 572291.20")
+    
     tester.loadAndAddTest("/Geocode/txt?", "SearchText=Hromadova%202741%20Kladno", "1033052.61, 766195.05")
+    tester.loadAndAddTest("/Geocode/txt?", "SearchText=Pra%C5%BEsk%C3%A1%20848%20Rokycany", "1072374.74, 805063.63")
+    tester.loadAndAddTest("/Geocode/txt?", "SearchText=M%C3%A1chova%2030/13%20Vejprty", "982913.93, 833696.81")
+    tester.loadAndAddTest("/Geocode/txt?", "SearchText=SearchText=Blahoslavova%201710%20Louny", "1007250.67, 781971.94")
+    
     tester.loadAndAddTest("/Geocode/txt?", "Street=Hromadova&Locality=Kladno&HouseNumber=2741&ZIPCode=27201&LocalityPart=Kladno", "1033052.61, 766195.05")
+    tester.loadAndAddTest("/Geocode/txt?", "Street=Mari%C3%A1nsk%C3%A1&Locality=Su%C5%A1ice&HouseNumber=67&ZIPCode=34201&LocalityPart=Su%C5%A1ice%20III", "1128217.51, 820609.28")
+    tester.loadAndAddTest("/Geocode/txt?", "Street=Dlouh%C3%A1&Locality=Terez%C3%ADn&HouseNumber=22&ZIPCode=41155&LocalityPart=Terez%C3%ADn", "993630.00, 755650.00")
+    tester.loadAndAddTest("/Geocode/txt?", "Street=Osadn%C3%AD&Locality=Rumburk&HouseNumber=1456&ZIPCode=40801&LocalityPart=Rumburk%201&OrientationNumber=12", "947618.24, 719309.30")
     tester.closeSection()
 
     if testerParam == None:
