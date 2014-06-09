d2ebc5e154963c47f8ff71194a4ed736bb46bf6c
raugustyn <raugustyn@post.cz>
2014-06-09 11:18:46 +0200

    Vytvoření adresáře Templates

---
+++ Templates/RUIANServices/rest_config_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Local configuration file, don't deploy to central repository GIT
+
+# CONFIGURATION
+
+SERVER_HTTP = 'www.vugtk.cz'
+PORT_NUMBER = 80
+SERVICES_WEB_PATH = "euradin/services/rest.py/"
+SERVER_PATH_DEPTH = 0
+HTMLDATA_URL = '/euradin/_euradin_services/'
+
+# END OF CONFIGURATION
