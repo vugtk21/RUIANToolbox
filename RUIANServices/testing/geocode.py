a0592edeeea85d76350e69fdf9f20d8b144a85d0
vacektom <mr.vaca@seznam.cz>
2014-06-20 09:59:25 +0200

    doplněny i příklady testů zadávání adres s výstupem do jednoho řádku

--- RUIANServices/testing/geocode.py
+++ RUIANServices/testing/geocode.py
@@ -17,37 +17,37 @@ def test(testerParam = None):
                  Textový řetězec adresy může být nestandardně formátován nebo může být i neúplný.""",
                 "Compiling person", "Tester")
 
-    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=1408739", "1033052.61, 766195.05")
-    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=20388802", "1056492.96, 529426.07")
-    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=8123934", "1098618.98, 568885.13")
-    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=8150656", "1086263.12, 572291.20")
-    tester.loadAndAddTest("/Geocode/txt?", "AddressPlaceId=140k", "")
+    tester.loadAndAddTest("/Geocode/text?", "AddressPlaceId=1408739", "1033052.61, 766195.05")
+    tester.loadAndAddTest("/Geocode/text?", "AddressPlaceId=20388802", "1056492.96, 529426.07")
+    tester.loadAndAddTest("/Geocode/text?", "AddressPlaceId=8123934", "1098618.98, 568885.13")
+    tester.loadAndAddTest("/Geocode/text?", "AddressPlaceId=8150656", "1086263.12, 572291.20")
+    tester.loadAndAddTest("/Geocode/text?", "AddressPlaceId=140k", "")
  
     
-    tester.loadAndAddTest("/Geocode/txt?", "SearchText=Blahoslavova%201710,%20Louny", "1007250.67, 781971.94")
-    tester.loadAndAddTest("/Geocode/txt?", "SearchText=U%20Kam,%20Vys", "1051248.61, 759804.26\n1051222.31, 759801.78")
-    tester.loadAndAddTest("/Geocode/txt?", "SearchText=Mramorov%C3%A1,%20Tet%C3%ADn", "1055481.79, 767602.15\n1055502.63, 767570.69\n1055447.02, 767576.82")
-    tester.loadAndAddTest("/Geocode/txt?", "SearchText=12", "")
+    tester.loadAndAddTest("/Geocode/text?", "SearchText=Blahoslavova%201710,%20Louny", "1007250.67, 781971.94")
+    tester.loadAndAddTest("/Geocode/text?", "SearchText=U%20Kam,%20Vys", "1051248.61, 759804.26\n1051222.31, 759801.78")
+    tester.loadAndAddTest("/Geocode/text?", "SearchText=Mramorov%C3%A1,%20Tet%C3%ADn", "1055481.79, 767602.15\n1055502.63, 767570.69\n1055447.02, 767576.82")
+    tester.loadAndAddTest("/Geocode/text?", "SearchText=12", "")
     
     
-    tester.loadAndAddTest("/Geocode/txt?", "Street=Hromadova&Locality=Kladno&HouseNumber=2741&ZIPCode=27201&LocalityPart=Kladno", "1033052.61, 766195.05")
-    tester.loadAndAddTest("/Geocode/txt?", "Street=Mari%C3%A1nsk%C3%A1&Locality=Su%C5%A1ice&HouseNumber=67&ZIPCode=34201&LocalityPart=Su%C5%A1ice%20III", "1128217.51, 820609.28")
-    tester.loadAndAddTest("/Geocode/txt?", "Street=Dlouh%C3%A1&Locality=Terez%C3%ADn&HouseNumber=22&ZIPCode=41155&LocalityPart=Terez%C3%ADn", "993630.00, 755650.00")
-    tester.loadAndAddTest("/Geocode/txt?", "Street=Osadn%C3%AD&Locality=Rumburk&HouseNumber=1456&ZIPCode=40801&LocalityPart=Rumburk%201&OrientationNumber=12", "947618.24, 719309.30")
+    tester.loadAndAddTest("/Geocode/text?", "Street=Hromadova&Locality=Kladno&HouseNumber=2741&ZIPCode=27201&LocalityPart=Kladno", "1033052.61, 766195.05")
+    tester.loadAndAddTest("/Geocode/text?", "Street=Mari%C3%A1nsk%C3%A1&Locality=Su%C5%A1ice&HouseNumber=67&ZIPCode=34201&LocalityPart=Su%C5%A1ice%20III", "1128217.51, 820609.28")
+    tester.loadAndAddTest("/Geocode/text?", "Street=Dlouh%C3%A1&Locality=Terez%C3%ADn&HouseNumber=22&ZIPCode=41155&LocalityPart=Terez%C3%ADn", "993630.00, 755650.00")
+    tester.loadAndAddTest("/Geocode/text?", "Street=Osadn%C3%AD&Locality=Rumburk&HouseNumber=1456&ZIPCode=40801&LocalityPart=Rumburk%201&OrientationNumber=12", "947618.24, 719309.30")
     
-    tester.loadAndAddTest("/Geocode/txt?", "HouseNumber=980f", "")
-    tester.loadAndAddTest("/Geocode/txt?", "HouseNumber=f", "")
+    tester.loadAndAddTest("/Geocode/text?", "HouseNumber=980f", "")
+    tester.loadAndAddTest("/Geocode/text?", "HouseNumber=f", "")
     
-    tester.loadAndAddTest("/Geocode/txt?", "ZIPCode=14000a", "")
-    tester.loadAndAddTest("/Geocode/txt?", "ZIPCode=a", "")
+    tester.loadAndAddTest("/Geocode/text?", "ZIPCode=14000a", "")
+    tester.loadAndAddTest("/Geocode/text?", "ZIPCode=a", "")
     
-    tester.loadAndAddTest("/Geocode/txt?", "OrientationNumber=12a", "")
-    tester.loadAndAddTest("/Geocode/txt?", "OrientationNumber=a", "")
+    tester.loadAndAddTest("/Geocode/text?", "OrientationNumber=12a", "")
+    tester.loadAndAddTest("/Geocode/text?", "OrientationNumber=a", "")
     
-    tester.loadAndAddTest("/Geocode/txt?", "RecordNumber=14d", "")
-    tester.loadAndAddTest("/Geocode/txt?", "RecordNumber=d", "")
+    tester.loadAndAddTest("/Geocode/text?", "RecordNumber=14d", "")
+    tester.loadAndAddTest("/Geocode/text?", "RecordNumber=d", "")
     
-    tester.loadAndAddTest("/Geocode/txt?", "", "")
+    tester.loadAndAddTest("/Geocode/text?", "", "")
     
     
     
