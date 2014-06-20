6d73f2bd72eb8104cc5f9b5f15188f8c81ce0192
vacektom <mr.vaca@seznam.cz>
2014-06-20 08:50:43 +0200

    upraveny testy i pro výstup "text do řádku"

--- RUIANServices/testing/nearbyaddresses.py
+++ RUIANServices/testing/nearbyaddresses.py
@@ -25,21 +25,25 @@ zadaných souřadnic do určité vzdálenosti. Vrací záznamy databáze RÚIAN 
         #result = urllib.quote(codecs.encode(result, "utf-8"))
         tester.addTest(path, result, expectedValue, "")
 
-    addTest("/NearbyAddresses/txt/655180/1030800/50", "č.p. 22, 50315 Pšánky")
-    addTest("/NearbyAddresses/txt/625350/1025770/200", "č.p. 54, 55203 Říkov")
+    addTest("/NearbyAddresses/textToOneRow/655180/1030800/50", "č.p. 22, 50315 Pšánky")
+    addTest("/NearbyAddresses/textToOneRow/625350/1025770/200", "č.p. 54, 55203 Říkov")
+    addTest("/NearbyAddresses/textToOneRow/724948/1007742/65? ", "č.p. 47, 27735 Kanina\nč.p. 42, 27735 Kanina")
+    addTest("/NearbyAddresses/textToOneRow/560670/1026662/80", "Kamenička č.ev. 31, 79069 Bílá Voda")
+    addTest("/NearbyAddresses/textToOneRow/697180/1066880/120", "Červený Hrádek 44, 28504 Bečváry")
     
-    addTest("/NearbyAddresses/text/724948/1007742/65? ", "č.p. 42, 27735 Kanina\nč.p. 47, 27735 Kanina")
+    addTest("/NearbyAddresses/text/655180/1030800/50", "č.p. 22\n50315 Pšánky")
+    addTest("/NearbyAddresses/text/625350/1025770/200", "č.p. 54\n55203 Říkov")   
+    addTest("/NearbyAddresses/text/724948/1007742/65? ", "č.p. 47\n27735 Kanina\nč.p. 42\n27735 Kanina")
+    addTest("/NearbyAddresses/text/560670/1026662/80", "Kamenička č.ev. 31\n79069 Bílá Voda")
+    addTest("/NearbyAddresses/text/697180/1066880/120", "Červený Hrádek 44\n28504 Bečváry")
     
-    addTest("/NearbyAddresses/txt/560670/1026662/80", "Kamenička č. ev. 31, 79069 Bílá Voda")
-    addTest("/NearbyAddresses/txt/697180/1066880/120", "Červený Hrádek 44, 28504 Bečváry")
-    
-    addTest("/NearbyAddresses/txt/a/1025770/200", "")
-    addTest("/NearbyAddresses/txt/625350/b/200", "")
-    addTest("/NearbyAddresses/txt/625350/1025770/c", "")
-    addTest("/NearbyAddresses/txt/a/b/c", "")
-    addTest("/NearbyAddresses/txt/a/b/200", "")
-    addTest("/NearbyAddresses/txt/625350/b/c", "")
-    addTest("/NearbyAddresses/txt/a/1025770/c", "")
+    addTest("/NearbyAddresses/text/a/1025770/200", "")
+    addTest("/NearbyAddresses/text/625350/b/200", "")
+    addTest("/NearbyAddresses/text/625350/1025770/c", "")
+    addTest("/NearbyAddresses/text/a/b/c", "")
+    addTest("/NearbyAddresses/text/a/b/200", "")
+    addTest("/NearbyAddresses/text/625350/b/c", "")
+    addTest("/NearbyAddresses/text/a/1025770/c", "")
     
     
     tester.closeSection()
