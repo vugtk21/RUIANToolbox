40f66f2628d1c9d7f284209dded05491b6a9186f
raugustyn <raugustyn@post.cz>
2014-10-29 13:14:17 +0100

    Fixes #71

--- RUIANServices/BuildTables.sql
+++ RUIANServices/BuildTables.sql
@@ -192,14 +192,14 @@ ALTER TABLE ui_mop
   
 INSERT INTO ui_mop VALUES ('19','Praha 1',554782);
 INSERT INTO ui_mop VALUES ('27','Praha 2',554782);
-INSERT INTO ui_momc VALUES ('35','Praha 3',554782);
-INSERT INTO ui_momc VALUES ('43','Praha 4',554782);
-INSERT INTO ui_momc VALUES ('51','Praha 5',554782);
-INSERT INTO ui_momc VALUES ('60','Praha 6',554782);
-INSERT INTO ui_momc VALUES ('78','Praha 7',554782);
-INSERT INTO ui_momc VALUES ('86','Praha 8',554782);
-INSERT INTO ui_momc VALUES ('94','Praha 9',554782);
-INSERT INTO ui_momc VALUES ('108','Praha 10',554782);
+INSERT INTO ui_mop VALUES ('35','Praha 3',554782);
+INSERT INTO ui_mop VALUES ('43','Praha 4',554782);
+INSERT INTO ui_mop VALUES ('51','Praha 5',554782);
+INSERT INTO ui_mop VALUES ('60','Praha 6',554782);
+INSERT INTO ui_mop VALUES ('78','Praha 7',554782);
+INSERT INTO ui_mop VALUES ('86','Praha 8',554782);
+INSERT INTO ui_mop VALUES ('94','Praha 9',554782);
+INSERT INTO ui_mop VALUES ('108','Praha 10',554782);
 
 -- ##################################################
 -- This script creates full text search tables
