250ed5cf7aeabfb08772343d52c68f92de62b562
Radek Augustýn <raugustyn@post.cz>
2014-12-02 12:37:21 +0100

    Doplnění popisu v dávce downloadu a záměna obrázku v dokumentaci.

--- DownloadRUIAN.bat
+++ DownloadRUIAN.bat
@@ -1,8 +1,12 @@
 @echo off
 echo Tento skript stahuje data z VDP
 echo -------------------------------
-echo Pro informaci o prubehu stahovani, sledujte obsah souboru DownloadRUIAN.log a DownloadRUIANErr.log, pripadne ostatni logovaci soubory v adresari se stazenymi daty.
+echo Pro informaci o prubehu stahovani, sledujte obsah souboru DownloadRUIAN.log
+echo a DownloadRUIANErr.log, echo pripadne ostatni logovaci soubory v adresari se
+echo stazenymi daty.
 echo Download muze trvat pri pomalem pripojeni k internetu nekolik desitek minut.
+echo Pokud je konfigurace nastavena tak, aby ihned po stazeni byla data importovana
+echo do databaze, muze byt cely ukoncen az za nekolik hodin.
 echo -------------------------------
 echo !!!Nezavirejte toto okno do ukonceni davky!!!
 cd RUIANDownloader
