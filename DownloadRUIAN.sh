e34276d08045d0a449b97256587a203f63469cfe
Jiri Hybek <jiri@hybek.cz>
2018-02-06 14:46:32 +0100

    Add lost message

--- DownloadRUIAN.sh
+++ DownloadRUIAN.sh
@@ -5,6 +5,8 @@ echo Download muze trvat pri pomalem pripojeni k internetu nekolik desitek minut
 echo Pokud je konfigurace nastavena tak, aby ihned po stazeni byla data importovana
 echo do databaze, muze byt cely ukoncen az za nekolik hodin.
 echo -------------------------------
+echo !!!Nezavirejte toto okno do ukonceni skriptu!!!
+
 cd downloader
 python downloadruian.py
 cd ..
--- ImportRUIAN.sh
+++ ImportRUIAN.sh
@@ -2,6 +2,7 @@
 echo Tento skript importuje data stazena z VDP do databaze.
 echo ------------------------------------------------------
 echo !!!Nezavirejte toto okno do ukonceni skriptu!!!
+
 cd importer
 python importruian.py
 cd ..\
